2a7a87b089d23fec9f5a70c9f278a869bfd1ed91
chajr <chajr@bluetree.pl>
2018-10-05 18:01:11 +0200

    Added class for shell output

---
+++ src/Performance/Output/Shell.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Benchmark\Performance\Output;
+
+class Shell
+{
+    /**
+     * prepare view and display list of markers, their times and percentage values
+     */
+    public function formatOutput(array $output, callable $formatter) : string
+    {
+        return '';
+    }
+}
