8b4f63475eac882a17f9ab1639fb0ecc53dd2272
chajr <chajr@bluetree.pl>
2018-10-13 17:48:21 +0200

    Basic support of benchmark template

--- src/Performance/Output/Html.php
+++ src/Performance/Output/Html.php
@@ -2,14 +2,38 @@
 
 namespace Benchmark\Performance\Output;
 
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 class Html
 {
     /**
+     * @var \Twig\TemplateWrapper
+     */
+    protected $template;
+
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(__DIR__ . '/../../template');
+        $env = new Environment($loader);
+        $this->template = $env->load('html-output.html');
+
+//        echo $this->template->render(['total_time' => 'adsfdasfsdfdsf']);
+//        exit;
+    }
+
+    /**
      * prepare view and display list of markers, their times and percentage values
      */
-    public function formatOutput(array $output, callable $formatter) : string
+    public function formatOutput(array $output) : string
     {
-
+        return $this->template->render([
+            'total_time' => $output['total_rune_time'],
+            'total_memory' => $output['total_memory'],
+            'markers' => $output['markers']
+        ]);
+        
+        
         $display = '<div style="
         color: #FFFFFF;
         background-color: #3d3d3d;
@@ -24,39 +48,14 @@ class Html
             Total memory usage: ' . $output['total_memory'] . '<br /><br />';
         $display .= 'Marker times:<br /><table style="width:100%">'."\n";
 
-        if (isset($outputp['markers'])) {
+        if (isset($output['markers'])) {
             foreach ($output['markers'] as $marker) {
-                if ($marker['marker_color']) {
-                    $additionalColor = 'background-color:#' . dechex($marker['marker_color']);
-                } else {
-                    $additionalColor = '';
-                }
-
-                if ($marker['marker_time'] === '') {
-                    $time = '-';
-                    $percent = '-';
-                    $ram = '-';
-                } else {
-                    $ram = ($marker['marker_memory'] - self::$sessionMemoryStart) / 1024;
-                    $ram = number_format($ram, 3, ',', '');
-                    $percent = ($marker['marker_time'] / $total) *100000;
-                    $percent = number_format($percent, 5);
-                    $time = number_format(
-                        $marker['marker_time'] *1000,
-                        5,
-                        '.',
-                        ' '
-                    );
-                    $time .= ' ms';
-                    $percent .= ' %';
-                    $ram .= ' kB';
-                }
-
+                $additionalColor = $marker['color'] ?: 'background-color:#' . dechex($marker['color']);
                 $display .= '<tr style="' . $additionalColor . '">
-                <td style="width:40%;color:#fff">' . $marker['marker_name'] . '</td>' . "\n";
-                $display .= '<td style="width:20%;color: #fff;">' . $time . '</td>'."\n";
-                $display .= '<td style="width:20%;color: #fff;">' . $percent . '</td>'."\n";
-                $display .= '<td style="width:20%;color:#fff">' . $ram . '</td>
+                <td style="width:40%;color:#fff">' . $marker['name'] . '</td>' . "\n";
+                $display .= '<td style="width:20%;color: #fff;">' . $marker['time'] . '</td>'."\n";
+                $display .= '<td style="width:20%;color: #fff;">' .  $marker['percentage'] . '</td>'."\n";
+                $display .= '<td style="width:20%;color:#fff">' . $marker['memory'] . '</td>
                 </tr>' . "\n";
             }
         }
